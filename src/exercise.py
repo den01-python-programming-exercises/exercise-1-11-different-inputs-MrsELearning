a6379d67d8134c8d9f677ee3a804092501d1d817
MrsElakeside <>
2021-05-13 13:55:20 +0000

    correct spelling

--- src/exercise.py
+++ src/exercise.py
@@ -1,12 +1,12 @@
 def main():
     #write your code below this line
     string1 = input("Give a string:")
-    string2 = input("Give an interger:")
+    string2 = input("Give an integer:")
     string3 = input("Give a float:")
     string4 = input("Give a boolean:")
 
     print("You gave the string "+ string1)
-    print("You gave the interger "+ string2)
+    print("You gave the integer "+ string2)
     print("You gave the float " + string3)
     print("You gave the boolean " + string4)
 
