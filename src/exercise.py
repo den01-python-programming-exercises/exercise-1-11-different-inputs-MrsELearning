47c7cce4d86e7fa9e8fb318a8ea2f38162f3ea64
MrsElakeside <>
2021-05-13 13:52:34 +0000

    took in input and sent it out again

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,19 @@
 def main():
     #write your code below this line
+    string1 = input("Give a string:")
+    string2 = input("Give an interger:")
+    string3 = input("Give a float:")
+    string4 = input("Give a boolean:")
+
+    print("You gave the string "+ string1)
+    print("You gave the interger "+ string2)
+    print("You gave the float " + string3)
+    print("You gave the boolean " + string4)
+
+
+
+
+    
 
 if __name__ == '__main__':
     main()
